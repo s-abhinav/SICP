8bec1cdf963d20fbb831767daaad99bb5fa9ee58
Abhinav <abhinav1587@gmail.com>
2017-05-09 16:48:05 +0000

    Add Exercises 1.29 and 1.30

---
+++ 1.30.scm
@@ -0,0 +1,34 @@
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+	result
+	(iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+(define (inc n) (+ n 1))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(define (cube x)
+  (* x x x))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b) dx))
+
+(define (simpsons-rule f a b n)
+  (define h (/ (- b a) n))
+  (define (y k)
+    (* (cond ((= k 0) 1)
+	     ((= k n) 1)
+	     ((odd? k) 4)
+	     ((even? k) 2))
+       (f (+ a (* k h)))))
+  (*
+   (/ h 3)
+   (sum y 0 inc n)))
+
+;; (simpsons-rule cube 0 1 1000)
+;; 1/4
+
