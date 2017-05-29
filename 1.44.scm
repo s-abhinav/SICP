8b7c117775d9a19abb63143230ddc919ad098993
Abhinav <abhinav1587@gmail.com>
2017-05-29 05:14:01 +0000

    Add Exercise 1.44

--- 1.44.scm
+++ 1.44.scm
@@ -1,6 +1,10 @@
+;; Exercise 1.42
+
 (define (compose f g)
   (lambda (x)
     (f (g x))))
+
+;; Exercise 1.43
 
 ;; Iterative process
 (define (repeated f n)
@@ -20,3 +24,17 @@
 
 ;; ((repeated square 2) 5)
 ;; => 625
+
+;; Exercise 1.44
+
+(define dx 0.00001)
+
+(define (smooth f)
+  (lambda (x)
+    (/ (+
+	(f (- x dx))
+	(f x)
+	(f (+ x dx)))
+       3)))
+
+(((repeated smooth 5) square) 4) ;; requires revisiting. TODO: find function and plot difference in graph to show smoothing.
