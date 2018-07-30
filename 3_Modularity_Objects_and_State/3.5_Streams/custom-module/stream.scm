8fe7f9caa7850153a3c98efafa076fa53d6f549c
Abhinav <abhinav1587@gmail.com>
2018-07-30 16:16:37 +0000

    Add 3.56

--- 3_Modularity_Objects_and_State/3.5_Streams/custom-module/stream.scm
+++ 3_Modularity_Objects_and_State/3.5_Streams/custom-module/stream.scm
@@ -8,7 +8,9 @@
 	   add-streams
 	   integers
 	   ones
-	   mul-streams))
+	   scale-stream
+	   merge
+	   ))
 
 (define the-empty-stream '())
 
@@ -60,3 +62,21 @@
   (cons-stream 1 (add-streams ones integers)))
 
 (define ones (cons-stream 1 ones))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+	((stream-null? s2) s1)
+	(else
+	 (let ((s1car (stream-car s1))
+	       (s2car (stream-car s2)))
+	   (cond ((< s1car s2car)
+		  (cons-stream s1car (merge (stream-cdr s1) s2)))
+		 ((> s1car s2car)
+		  (cons-stream s2car (merge s1 (stream-cdr s2))))
+		 (else
+		  (cons-stream s1car
+			       (merge (stream-cdr s1)
+				      (stream-cdr s2)))))))))
