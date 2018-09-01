e4f3c9dd99b2fc689b5b4e87f95b4f3f1339730e
Abhinav <abhinav1587@gmail.com>
2018-09-01 01:33:04 -0400

    Add 3.71 Fix merge weighted. Do not skip duplicate weights.

--- 3_Modularity_Objects_and_State/3.5_Streams/custom-module/stream.scm
+++ 3_Modularity_Objects_and_State/3.5_Streams/custom-module/stream.scm
@@ -1,6 +1,7 @@
 (define-module (custom-module stream)
   :export (
 	   stream-null?
+	   the-empty-stream
 	   cons-stream
 	   stream-car
 	   stream-cdr
@@ -22,6 +23,8 @@
 	   interleave
 	   stream-filter
 	   pairs
+	   pairs-ordered
+	   merge-weighted
 	   ))
 
 (define the-empty-stream '())
@@ -34,7 +37,7 @@
     ((cons-stream a b)
      (cons a (delay b)))))
 
-(define (stream-car stream) 
+(define (stream-car stream)
   (car stream))
 
 (define (stream-cdr stream) 
@@ -172,3 +175,30 @@
                   (list (stream-car s) x))
                 (stream-cdr t))
     (pairs (stream-cdr s) (stream-cdr t)))))
+
+(define (merge-weighted s1 s2 weight)
+  (cond ((stream-null? s1) s2)
+	((stream-null? s2) s1)
+	(else
+	 (let ((s1car (stream-car s1))
+	       (s2car (stream-car s2)))
+	   (cond ((< (weight s1car) (weight s2car))
+		  (cons-stream s1car
+			       (merge-weighted (stream-cdr s1) s2 weight)))
+		 ((> (weight s1car) (weight s2car))
+		  (cons-stream s2car
+			       (merge-weighted s1 (stream-cdr s2) weight)))
+		 (else
+		  (cons-stream s1car (cons-stream s2car
+				      (merge-weighted (stream-cdr s1)
+						      (stream-cdr s2) weight)))))))))
+
+(define (pairs-ordered s t weight)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (merge-weighted
+    (stream-map (lambda (x)
+		  (list (stream-car s) x))
+		(stream-cdr t))
+    (pairs-ordered (stream-cdr s) (stream-cdr t) weight)
+    weight)))
