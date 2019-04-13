d3a0526e9539b02d7f89807f327035cf33b66ed1
Abhinav <1825805+s-abhinav@users.noreply.github.com>
2019-04-12 22:57:34 -0400

    Update extract-labels with the version from Exercise 5.8.

--- 5_Computing_with_Register_Machines/5.2_A_Register-Machine_Simulator/machine.scm
+++ 5_Computing_with_Register_Machines/5.2_A_Register-Machine_Simulator/machine.scm
@@ -148,14 +148,17 @@
       (extract-labels
        (cdr text)
        (lambda (insts labels)
-	 (let ((next-inst (car text)))
-	   (if (symbol? next-inst)
-	       (receive
-		   insts
-		   (cons (make-label-entry next-inst insts) labels))
-	       (receive
-		   (cons (make-instruction next-inst) insts)
-		   labels)))))))
+         (let ((next-inst (car text)))
+           (if (symbol? next-inst)
+               (if (assoc next-inst labels)
+                   (error "Duplicate label -- ASSEMBLE" next-inst)
+                (receive
+                    insts
+                    (cons (make-label-entry next-inst insts) labels)))
+               (receive
+                   (cons (make-instruction next-inst) insts)
+                   labels)))))))
+
 
 (define (lookup-label labels label-name)
   (let ((val (assoc label-name labels)))
